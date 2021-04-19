b3f617351655724704da8943f4c752fadfbeaef3
ofinadir <rofynadzir1@gmail.com>
2021-04-19 14:23:00 +0700

    change anchor from nav

--- src/scripts/components/navbar-barlland.js
+++ src/scripts/components/navbar-barlland.js
@@ -7,7 +7,7 @@ class NavbarBarlland extends HTMLElement {
     this.innerHTML =`
       <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
         <div class="container">
-          <a class="navbar-brand" href="#"><img src="./logo/logo-dark.svg" class="lazyload nav__brand-logo" alt="logo-Barlland" /></a>
+          <a class="navbar-brand" href="index.html"><img src="./logo/logo-dark.svg" class="lazyload nav__brand-logo" alt="logo-Barlland" /></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
